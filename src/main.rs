9f5a09aec32d9274edaf045bfce0dda14578309d
FreeMasen <RFM@Herman.local>
2017-12-28 07:25:03 -0600

    add parse code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io::*;
 use std::fs::{File};
 use std::path::{PathBuf};
-use std::Wrapping;
+// use std::Wrapping;
 
 fn main() {
     let path = PathBuf::from("db.sqlite");
@@ -11,13 +11,12 @@ fn main() {
     let first_16 = buf.get(0..16).expect("Unable to slice off first 16 bytes");
     let magic_string = String::from_utf8(first_16.to_vec()).expect("Unable to convert from utf8 to magic string");
     let next_two = buf.get(16..18).expect("Unable to slice of next two");
+    println!("next_two: {:?}", next_two);
     let page_size = parse_u16(next_two[0], next_two[1]);
-
+    println!("page size: {:?}", page_size);
 }
 
 pub fn parse_u16(big_end: u8, little_end: u8) -> u16 {
-    (
-    Wrapping(big_end    as u16) << 8 |
-    Wrapping(little_end as u16)
-    ).0
+    (big_end    as u16) << 8 |
+    little_end as u16
 }
