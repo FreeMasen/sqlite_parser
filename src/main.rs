602b970dcaea648284acc3f537c174aa0ecb16b1
Robert Masen <r.f.masen@gmail.com>
2022-11-06 12:47:37 -0600

    revert reader stuff

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,11 @@ use std::io::{BufReader, Read, Cursor, Seek, SeekFrom};
 fn main() {
     let file = std::fs::File::open("database.sqlite").unwrap();
     let mut reader = BufReader::new(file);
-    // let cursor = Cursor::new(file);
-    
-    loop {
-        let _new_offset = reader.seek(SeekFrom::Start(0)).unwrap();
-        let mut buf = [0u8;100];
-        let ct = reader.read(&mut buf).unwrap();
-        if ct != 100 {
-            panic!("Unable to read the first 100 bytes of database.sqlite");
-        }
-        let header = parse_header(&buf).unwrap();
-        println!("{:#?}", header);
-        std::thread::sleep(std::time::Duration::from_secs(1));
+    let mut buf = [0u8;100];
+    let ct = reader.read(&mut buf).unwrap();
+    if ct != 100 {
+        panic!("Unable to read the first 100 bytes of database.sqlite");
     }
+    let header = parse_header(&buf).unwrap();
+    println!("{:#?}", header);
 }
